dd804df603005762149c22b5f56f4c677244deee
hellossp <sitansupanda791@gmail.com>
2026-03-29 15:31:22 +0530

    first deploy

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-3xl font-bold text-center mt-10">
+      kabhi na kabhi toh portfolio banunga hi ese mene 2024 mein socha tha 😂😂
+    </h1>
+  );
+}
+
+export default App;
